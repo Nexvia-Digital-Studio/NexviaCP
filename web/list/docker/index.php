b4be8b1b9400bd2095070ba2542b9e37e0215e10
Nexvia Digital Studio <info@nexviadigital.com>
2026-08-02 18:49:50 +0300

    feat: integrate official Portainer CE Docker engine directly with v-add-sys-portainer installer and direct redirect

--- web/list/docker/index.php
+++ web/list/docker/index.php
@@ -2,7 +2,13 @@
 session_start();
 require_once "../../inc/main.php";
 
-$TAB = "DOCKER";
+// Redirect directly to official Portainer CE Application
+$host = $_SERVER['HTTP_HOST'];
+// Remove port if present in HTTP_HOST
+if (strpos($host, ':') !== false) {
+    $host = explode(':', $host)[0];
+}
 
-// Render page with Docker Manager UI
-render_page($user, $TAB, "list_docker");
+// Redirect to official Portainer CE on port 9000
+header("Location: http://" . $host . ":9000");
+exit();
